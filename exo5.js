2eae24b8ab324d0b679d35d2a885c4831a4bd96f
Pierre Schuvey <pierre.schuveye2n@gmail.com>
2017-10-25 14:08:36 +0200

    fin de l'exercice 5 de la partie 2 du JS

---
+++ exo5.js
@@ -0,0 +1,20 @@
+function changeimage1(src)
+{
+  document.getElementById('image1').src='image1_2.jpg';
+}
+function changeimage2(src)
+{
+  document.getElementById('image2').src='image2_2.jpg';
+}
+function changeimage3(src)
+{
+  document.getElementById('image3').src='image3_2.jpg';
+}
+function changeimage4(src)
+{
+  document.getElementById('image4').src='image4_2.jpg';
+}
+function changeimage5(src)
+{
+  document.getElementById('image5').src='image5_2.jpg';
+}
