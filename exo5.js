308350fb187ea730b7f601f86b112c867b8fdd3d
Pierre Schuvey <pierre.schuveye2n@gmail.com>
2017-10-26 15:59:18 +0200

    Bonus de l'exercice

--- exo5.js
+++ exo5.js
@@ -1,40 +1,4 @@
-function changeimage1(src)
+function changePicture(image, url)
 {
-  document.getElementById('image1').src='image1_2.jpg';
-}
-function changeimage2(src)
-{
-  document.getElementById('image2').src='image2_2.jpg';
-}
-function changeimage3(src)
-{
-  document.getElementById('image3').src='image3_2.jpg';
-}
-function changeimage4(src)
-{
-  document.getElementById('image4').src='image4_2.jpg';
-}
-function changeimage5(src)
-{
-  document.getElementById('image5').src='image5_2.jpg';
-}
-function changeimage6(src)
-{
-  document.getElementById('image1').src='image1.jpg';
-}
-function changeimage7(src)
-{
-  document.getElementById('image2').src='image2.jpg';
-}
-function changeimage8(src)
-{
-  document.getElementById('image3').src='image3.jpg';
-}
-function changeimage9(src)
-{
-  document.getElementById('image4').src='image4.jpg';
-}
-function changeimage10(src)
-{
-  document.getElementById('image5').src='image5.jpg';
+    image.setAttribute('src', url);
 }
